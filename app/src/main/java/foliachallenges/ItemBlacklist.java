b22874043a34bf91341ae5b59d454aef0fd98826
dinushay <dinu@dinushay.de>
2026-01-05 02:22:48 +0100

    Update hardcoded blacklist

--- app/src/main/java/foliachallenges/ItemBlacklist.java
+++ app/src/main/java/foliachallenges/ItemBlacklist.java
@@ -7,8 +7,35 @@ import java.util.List;
 public class ItemBlacklist {
     public static final List<Material> HARDCODED_BLACKLIST = Arrays.asList(
         Material.AIR,
-        Material.BEDROCK,
+        Material.VOID_AIR,
+        Material.CAVE_AIR,
         Material.BARRIER,
-        Material.COMMAND_BLOCK
+        Material.STRUCTURE_VOID,
+        Material.BEDROCK,
+        Material.COMMAND_BLOCK,
+        Material.CHAIN_COMMAND_BLOCK,
+        Material.REPEATING_COMMAND_BLOCK,
+        Material.COMMAND_BLOCK_MINECART,
+        Material.STRUCTURE_BLOCK,
+        Material.JIGSAW,
+        Material.LIGHT,
+        Material.DEBUG_STICK,
+        Material.KNOWLEDGE_BOOK,
+        Material.REINFORCED_DEEPSLATE,
+        Material.END_PORTAL_FRAME,
+        Material.END_PORTAL,
+        Material.NETHER_PORTAL,
+        Material.END_GATEWAY,
+        Material.SPAWNER,
+        Material.BUDDING_AMETHYST,
+        Material.FROGSPAWN,
+        Material.FARMLAND,
+        Material.DIRT_PATH,
+        Material.INFESTED_STONE,
+        Material.WATER,
+        Material.LAVA,
+        Material.BUBBLE_COLUMN,
+        Material.FIRE,
+        Material.SOUL_FIRE
     );
-}
+} // # Items that are impossible to obtain for the challenge are added here from time to time. Please continue to contribute to this list.
